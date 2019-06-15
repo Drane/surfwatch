798f5e73a1bc33bfc8a232685b9f4f65a1601882
Jochen Szostek <jochen.szostek@economie.fgov.be>
2019-06-15 20:05:17 +0200

    Generated loopback 4 app

---
+++ src/index.ts
@@ -0,0 +1,16 @@
+import {SurfwatchApplication} from './application';
+import {ApplicationConfig} from '@loopback/core';
+
+export {SurfwatchApplication};
+
+export async function main(options: ApplicationConfig = {}) {
+  const app = new SurfwatchApplication(options);
+  await app.boot();
+  await app.start();
+
+  const url = app.restServer.url;
+  console.log(`Server is running at ${url}`);
+  console.log(`Try ${url}/ping`);
+
+  return app;
+}
